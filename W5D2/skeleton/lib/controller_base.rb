9ac3548bf06df9770702b9982c7fa05e9dbcd43c
Justin White <mizzou_fan10@hotmail.com>
2017-06-27 14:45:16 -0700

    Wrote method to render content

--- W5D2/skeleton/lib/controller_base.rb
+++ W5D2/skeleton/lib/controller_base.rb
@@ -8,11 +8,13 @@ class ControllerBase
 
   # Setup the controller
   def initialize(req, res)
-
+    @req = req
+    @res = res
   end
 
   # Helper method to alias @already_built_response
   def already_built_response?
+    @already_built_response
   end
 
   # Set the response status code and header
@@ -23,6 +25,10 @@ class ControllerBase
   # Set the response's content type to the given type.
   # Raise an error if the developer tries to double render.
   def render_content(content, content_type)
+    raise "double render error" if already_built_response?
+    res.write(content)
+    res['Content-Type'] = content_type
+    @already_built_response = true
   end
 
   # use ERB and binding to evaluate templates
