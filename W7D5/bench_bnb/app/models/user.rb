305a84fbb277e9c45c45ed203a4c5100c9ab424c
Jstuff36 <mizzou_fan10@hotmail.com>
2017-07-14 18:10:47 -0700

    Finished user model

--- W7D5/bench_bnb/app/models/user.rb
+++ W7D5/bench_bnb/app/models/user.rb
@@ -2,8 +2,30 @@ class User < ApplicationRecord
   validates :username, :password_digest, presence: true
   validates :password, length: {minimum: 6, allow_nil: true }
 
+  before_validation :ensure_session_token
+
+  def ensure_session_token
+    self.session_token ||= SecureRandom.urlsafe_base64(16)
+  end
+
   def password=(password)
     @password = password
-    self.password_digest
+    self.password_digest = BCrypt::Password.create(password)
   end
+
+  def reset_token!
+    self.session_token = SecureRandom.urlsafe_base64(16)
+    self.save!
+    self.session_token
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(self.password_digest).is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    return user if user && user.valid_password?(password)
+  end
+
 end
